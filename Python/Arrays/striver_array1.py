b34bc98b470054eec8d08f816e81d6cf9f77360e
Rayirth Soni <rayirth.soni@finbox.in>
2024-05-25 12:03:58 +0530

    solved all python array 1 brute solutions

--- Python/Arrays/striver_array1.py
+++ Python/Arrays/striver_array1.py
@@ -12,24 +12,46 @@ def get_smallest(my_list):
 
 
 def get_second_largest(my_list):
-    pass
+
+    for element in sorted(my_list)[-1::-1]:
+        if element != my_list[-1]:
+            return element
+
+    return 'All Elements are Equal'
 
 
 def get_second_smallest(my_list):
-    pass
+
+    for element in sorted(my_list)[1:]:
+        if element != my_list[0]:
+            return element
+
+    return 'All Elements are Equal'
 
 
 def is_sorted(my_list):
-    pass
+
+    for index, element in enumerate(my_list):
+        for next_element in my_list[index+1:]:
+            if element > next_element:
+                return False
+    return True
 
 
-def remove_duplicates_from_sorted():
-    pass
+def remove_duplicates_from_sorted(my_list):
+
+    return sorted(list(set(my_list)))
 
 
-my_list = [31, 4363, 234, 843, 13, 4, 854, 98, 56, 4335,
-           109, 59, 9898, 1943, -54, 234, 64, -324, -32, -11]
+my_list = [31, 13, -22, 82, 13, 4, 88, 99, 56, 67,
+           99, 59, 24, -23, -54, -9, 64, -3, -32, -11, -98, 98, -98]
 
-maxi = get_largest(my_list)
-print(f"Largest Number is : {maxi}")
-print(f"Smallest Number is : {maxi}")
+
+print(f"Largest Element: {get_largest(my_list)}")
+print(f"Smallest Element: {get_smallest(my_list)}")
+print(f"Second Largest Element: {get_second_largest(my_list)}")
+print(f"Seocnd Smallest Element: {get_second_smallest(my_list)}")
+print(f"Is Sorted ? : {is_sorted(my_list)}")
+print(f"Is Sorted ? : {is_sorted(my_list)}")
+print(f"Before removing duplicates\n{sorted(my_list)}\nAfter removing duplicates\n{
+      (remove_duplicates_from_sorted(my_list))}")
