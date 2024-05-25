b85944ae27dfcacf6acf53891a73ab442f71fb4e
Rayirth Soni <rayirth.soni@finbox.in>
2024-05-25 12:31:41 +0530

    added init file

---
+++ Python/Arrays/striver_array.py
@@ -0,0 +1,6 @@
+from utils.array import get_largest
+
+my_list = [28, -12, -43, 85, 98, 100, 99, -32, -98, -1, 4, 88,
+           -44, 11, 11, -45, 87, 98, 39, -23, 69, 19, -57, 13, -71]
+
+print(f'Largest Element is : {get_largest(my_list)}')
---
+++ utils/array.py
@@ -0,0 +1,14 @@
+def get_largest(my_list):
+    return sorted(my_list)[-1]
+
+
+def get_smallest(my_list):
+    return sorted(my_list)[0]
+
+
+def get_second_largest(my_list):
+    pass
+
+
+def get_second_smallest(my_list):
+    pass
