ef67ef03064b01a194515d6a61db1dee8a7d267e
Rayirth Soni <rayirth.soni@finbox.in>
2024-05-26 16:25:20 +0530

    added array functions

--- Python/utils/array_functions.py
+++ Python/utils/array_functions.py
@@ -1,14 +1,59 @@
 def get_largest(my_list):
+    """returns the largest element in a list
+
+    Args:
+        my_list (list): list for which you want largest element
+
+    Returns:
+        numeric: largest element in the list
+    """
     return sorted(my_list)[-1]
 
 
 def get_smallest(my_list):
+    """return the smallest element in a list
+
+    Args:
+        my_list (list): list for which you want smallest element
+
+    Returns:
+        numeric: smallest element in the list
+    """
     return sorted(my_list)[0]
 
 
 def get_second_largest(my_list):
-    pass
+    """returns the second largest element in the list
+
+    Args:
+        my_list (list): list for which you want second largest element
+
+    Returns:
+        numeric: second largest element in the list
+    """
+    
+    sorted_list = sorted(my_list)
+    
+    for element in sorted_list[::-1]:
+        if element != sorted_list[-1]:
+            return element
+    
+    return 'All elements are equal.'
 
 
 def get_second_smallest(my_list):
-    pass
+    """returns the second smallest element in the list
+
+    Args:
+        my_list (list): list for which you want second smallest element
+
+    Returns:
+        numeric: second smallest element in the list
+    """
+    sorted_list = sorted(my_list)
+
+    for element in sorted_list:
+        if element != sorted_list[0]:
+            return element
+
+    return 'All elements are equal.'
---
+++ Python/utils/striver_array.py
@@ -0,0 +1,13 @@
+from utils.array_functions import get_largest
+from utils.array_functions import get_second_largest
+from utils.array_functions import get_smallest
+from utils.array_functions import get_second_smallest
+
+
+print(get_largest([1,2,3,4,56,342,932,-2,-45]))
+
+print(get_smallest([1,2,3,4,56,342,932,-2,-45]))
+
+print(get_second_largest([1,2,3,4,56,342,932,-2,-45]))
+
+print(get_second_smallest([1,2,3,4,56,342,932,-2,-45]))
