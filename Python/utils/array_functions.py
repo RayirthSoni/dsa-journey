1bffaf7b63fe28e63c2d82685517d722b716adc8
Rayirth Soni <rayirth.soni@finbox.in>
2024-05-26 20:37:26 +0530

    added solution for is array sorted

--- Python/utils/array_functions.py
+++ Python/utils/array_functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def get_largest(my_list):
     """returns the largest element in a list
 
@@ -10,11 +11,11 @@ def get_largest(my_list):
         numeric: largest element in the list
     """
     maximum_element = -np.inf
-    
+
     for element in my_list:
         if element > maximum_element:
             maximum_element = element
-    
+
     return maximum_element
 
 
@@ -28,13 +29,12 @@ def get_smallest(my_list):
         numeric: smallest element in the list
     """
     minimum_element = np.inf
-    
+
     for element in my_list:
         if element < minimum_element:
             minimum_element = element
-    
-    return minimum_element
 
+    return minimum_element
 
 
 def get_second_largest(my_list):
@@ -48,14 +48,14 @@ def get_second_largest(my_list):
     """
     maximum_element = -np.inf
     second_maximum_element = -np.inf
-    
+
     for element in my_list:
         if element > maximum_element:
             second_maximum_element = maximum_element
             maximum_element = element
         elif element < maximum_element and element > second_maximum_element:
             second_maximum_element = element
-    
+
     return second_maximum_element
 
 
@@ -68,15 +68,43 @@ def get_second_smallest(my_list):
     Returns:
         numeric: second smallest element in the list
     """
-    
+
     minimum_element = np.inf
     second_minimum_element = np.inf
-    
+
     for element in my_list:
         if element < minimum_element:
             second_minimum_element = minimum_element
             minimum_element = element
         elif element > minimum_element and element < second_minimum_element:
             second_minimum_element = element
-            
+
     return second_minimum_element
+
+
+def is_array_sorted(my_list):
+    """checks if the list is sorted
+
+    Args:
+        my_list (list): list to be checked
+
+    Returns:
+        bool: True if list is sorted, False otherwise
+    """
+    size = len(my_list)
+    for index in range(size - 1):
+        if my_list[index] > my_list[index + 1]:
+            return False
+    return True
+
+
+def remove_duplicates_from_sorted(my_list):
+    """removes the duplicates from a sorted list
+
+    Args:
+        my_list (list): list from which you want to remove duplicates
+
+    Returns:
+        list: sorted list after removing duplicates
+    """
+    pass
