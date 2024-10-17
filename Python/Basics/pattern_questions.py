6cc92d23dceba6eac8e9cb33e18c233bd669ba2e
Rayirth Soni <rayirthsoni04@gmail.com>
2024-10-17 23:40:32 +0530

    update: square pattern added

--- Python/Basics/pattern_questions.py
+++ Python/Basics/pattern_questions.py
@@ -2,3 +2,10 @@
 This script contains questions on pattern questions based on looping concepts
 '''
 
+def get_square_pattern(n: int) -> None:
+    pattern = '*' * n
+    for _ in range(n):
+        print(pattern)
+
+pattern_size = int(input("Enter size of pattern: "))
+get_square_pattern(pattern_size)
