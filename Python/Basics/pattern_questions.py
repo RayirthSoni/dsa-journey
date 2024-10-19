932dce9a307aa695b0f78b8f13da455a1962c775
Rayirth Soni <rayirthsoni04@gmail.com>
2024-10-19 15:16:37 +0530

    update: square pattern brute and optimized

--- Python/Basics/pattern_questions.py
+++ Python/Basics/pattern_questions.py
@@ -6,3 +6,28 @@ This script contains questions on pattern questions based on looping concepts
 # pylint: disable=line-too-long
 
 
+def get_square_pattern_brute(n: int) -> None:
+    """
+    Function is used to print square pattern of stars in brute force way
+
+    Args:
+        n (int): Size of pattern
+    """
+    for i in range(n):
+        for j in range(n):
+            print("*", end="")
+        print()
+
+
+def get_square_pattern_optimal(n: int) -> None:
+    """
+    Function is used to print square pattern of stars in optimal way
+
+    Args:
+        n (int): Size of pattern
+    """
+    pattern = "*" * n
+    for _ in range(n):
+        print(pattern)
+
+
