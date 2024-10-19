00e7f90b6f89456d240f2fb29c58506a48eb9d61
Rayirth Soni <rayirthsoni04@gmail.com>
2024-10-19 13:03:01 +0530

    update: removed pattern utils

--- Python/Basics/pattern_questions.py
+++ Python/Basics/pattern_questions.py
@@ -1,15 +1,8 @@
-'''
+"""
 This script contains questions on pattern questions based on looping concepts
-'''
+"""
 
 # Ignore pylint warnings
 # pylint: disable=line-too-long
 
-from utils.patterns import Pattern
 
-
-# Pattern.Triangle.get_increasing_triangle_brute(5)
-# print('1' + str(int('1') + 1))
-# clPattern.Square.get_optimized_pattern(3)
-
-# print(range(1,1))
