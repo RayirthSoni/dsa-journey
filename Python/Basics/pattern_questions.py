0616811704737ab2a9c953152827a124489535b7
Rayirth Soni <rayirthsoni04@gmail.com>
2025-03-02 15:48:32 +0530

    main: number crown solved

--- Python/Basics/pattern_questions.py
+++ Python/Basics/pattern_questions.py
@@ -306,7 +306,18 @@ def get_number_crown_pattern(n):
             12  21
             123321
     """
-    pass
+    for i in range(1, n + 1):
+
+        for j in range(1, i + 1):
+            print(j, end="")
+
+        for j in range(2 * (n - i)):
+            print(" ", end="")
+
+        for j in range(i, 0, -1):
+            print(j, end="")
+
+        print()
 
 
 def get_increasing_number_triangle(n):
