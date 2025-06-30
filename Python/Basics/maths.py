c6caefeaece0233037cf3e2316b0fff551b07c09
Rayirth Soni <rayirthsoni04@gmail.com>
2025-07-01 00:06:27 +0530

    main: update: docstrings for is_palindrome

--- Python/Basics/maths.py
+++ Python/Basics/maths.py
@@ -18,10 +18,14 @@ def get_digit_count(number: int, apporach: str):
     Brute:
         Extract the count of digits in number by division by 10 till n > 0
 
+        Time Complexity = O(log n + 1)
+
 
     Optimized:
         To get count of digits in number by using log n + 1
         Here we will take log to base 10 as that will give digit count on intuition
+
+        Time Complexity = O(1) -> As we use a simple mathematical formula
 
 
     Args:
@@ -40,12 +44,56 @@ def get_digit_count(number: int, apporach: str):
     return cnt
 
 
-def get_reverse_number(number):
-    pass
+def get_reverse_number(number: int):
+    """Function is used to get reverse number of the given number
+
+    Args:
+        number (int): Number for which we need reverse number
+
+    Optimized:
+        Example = 1234
+        Output = 4321
+
+        1234 // 10 = 123 | 1234 % 10 = 4
+        123 // 10 = 12 | 123 % 10 = 3 + 4 * 10
+        and so on
+    """
+    reverse_number = 0
+    while number > 0:
+        remainder = number % 10
+        reverse_number = reverse_number * 10 + remainder
+        number = number // 10
+
+    return reverse_number
 
 
-def is_palindrome(number):
-    pass
+def is_palindrome(number: int):
+    """
+    Function is used to flag if number is a palindrome or not
+
+    
+    Args:
+        number (int): Number to check if palindrome or not
+
+
+    Optimized:
+        Example = 1221 
+        Reverse = 1221
+
+        reverse == input ; palindrome
+
+        Algorithm:
+            1. Compute reverse of the number
+            2. Check if equal to original or not
+    """
+    duplicate = number
+    reverse_number = 0
+    while number > 0:
+        last_digit = number % 10
+        reverse_number = reverse_number * 10 + last_digit
+        number = number // 10
+
+    return duplicate == reverse_number
 
 
 def get_gcd(a, b):
