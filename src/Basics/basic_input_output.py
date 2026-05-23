34a6bd26ca0d86731da2970af1c7b359921f74c6
RayirthSoni <rayirthsoni04@gmail.com>
2026-05-24 00:02:10 +0530

    main: add: new script for basic input and output exercises

---
+++ src/Basics/basic_input_output.py
@@ -0,0 +1,14 @@
+"""
+This script contains questions based on basic input and output.
+"""
+
+
+# Question 1: Write a program to print "Hello, World!" on the screen.
+print("Hello, World!")
+
+
+# Question 2: Write a program to print the sum of two numbers.
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+sum = num1 + num2
+print("The sum of the two numbers is: ", sum)
