acecd7340348ae1648b77c8be4ce4bc7843dfe1a
Rayirth Soni <77580711+RayirthSoni@users.noreply.github.com>
2026-05-24 01:08:49 +0530

    Add conditional statements script with age and grade checks
    Added a script with questions on conditional statements, including age check and grading system based on marks.

--- src/basics/conditional_statements.py
+++ src/basics/conditional_statements.py
@@ -1,3 +1,28 @@
 """
 This script contains questions based on conditional statements.
 """
+
+# Question 1: An indivial is adult or not.
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are an adult")
+else:
+    print("You are not an adult")
+
+
+# Question 2: Grade and Marks of a User
+marks = int(input("Enter your marks: "))
+
+if marks < 25:
+    print("Grade: F")
+elif marks <= 44:
+    print("Grade: E")
+elif marks <= 49:
+    print("Grade: D")
+elif marks <= 59:
+    print("Grade: C")
+elif marks <= 69:
+    print("Grade: B")
+elif marks <= 100:
+    print("Grade: A")
