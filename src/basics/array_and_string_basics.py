de72a94246f8695cf75494b44f54b8f5b67a0b19
RayirthSoni <rayirthsoni04@gmail.com>
2026-05-24 01:09:46 +0530

    format: improve code readability in array_and_string_basics.py
    Adjusted spacing in print statements for better readability and consistency throughout the script.

--- src/basics/array_and_string_basics.py
+++ src/basics/array_and_string_basics.py
@@ -4,7 +4,7 @@ This scripts contains basics on arrays and strings
 
 # Question 1: Length of string
 a = "Hello"
-print("length of string a =" ,len(a))
+print("length of string a =", len(a))
 
 # Question 2: Accessing Individual Characters
 for i in range(len(a)):
@@ -13,19 +13,21 @@ for i in range(len(a)):
 # Question 3: Passing, Returning and Assigning Strings
 original = "Hello"
 
+
 def modify_string(og: str) -> str:
     new = og
     new += " World"
     return new
 
+
 modified_string = modify_string(original)
 
 print("original ->", original)
-print("modified_string ->",modified_string)
+print("modified_string ->", modified_string)
 
 
 # Question 4: String Comparison
 chk1 = "abc"
 chk2 = "abc"
 
-print("chk1 == chk2 ->",chk1 == chk2)
+print("chk1 == chk2 ->", chk1 == chk2)
